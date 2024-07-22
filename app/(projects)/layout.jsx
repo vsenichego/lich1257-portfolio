d545d7e6bd5c67b3905addaac08d7793ba92cd82
vsenichego <andreygovorunn@gmail.com>
2024-07-22 23:02:37 +0300

    update layout.jsx, prepare to connect strapi cms as a backend, save progress

--- app/(projects)/layout.jsx
+++ app/(projects)/layout.jsx
@@ -10,7 +10,7 @@ const Layout = ({ children }) => {
             <header className="absolute p-4 z-10">
                 <Logo />
             </header>
-            <div>{children}</div>
+            {children}
             <footer className="absolute bottom-3 left-10 text-[40px] uppercase z-10">
                 <div className="flex flex-row items-center">
                     <span className="pr-1.5">/</span>
